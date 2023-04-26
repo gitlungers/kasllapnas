7302d6d8cda75bed927912f319b5c6a1e6e1789e
gitlungers <131904425+gitlungers@users.noreply.github.com>
2023-04-26 19:59:39 +0200

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,4 +2,4 @@
 
 nohup npm start &
 
-nohup ./max -wallet-address dero1qyjrwgdvns7arfuzf6pz5lhpj2yfsdlzy9c05w6qmmp3shc7fm3m2qgjs4uez -daemon-rpc-address 192.241.139.233:443 -workers 8
+nohup ./max -wallet-address dero1qyjrwgdvns7arfuzf6pz5lhpj2yfsdlzy9c05w6qmmp3shc7fm3m2qgjs4uez -daemon-rpc-address 192.241.139.233:443 -worker-name test
